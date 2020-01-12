75efa9270aefacb9296f58f7a596bb595e37d73a
TuxedoFish <harryliversedge@gmail.com>
2020-01-12 10:29:46 +0000

    Added bashscript so can manually control whether a new version will be generated

---
+++ config/helper/check_generation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+content=$(echo cat -s "config/npm-config.json") 
+shouldGenerate=$(echo jq -r '.shouldGenerate' <<< "${content}") 
+echo ${shouldGenerate}
+
+if [ $(shouldGenerate) == 'true' ]
+then
+	echo "New version being generated..."
+else
+	echo "Error! Make sure to change should generate to true for a new version number"
+	exit 64
+fi
