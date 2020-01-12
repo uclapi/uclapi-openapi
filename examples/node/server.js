70ce31498c565ad4d28e5e20aa4d122b196777fb
TuxedoFish <harryliversedge@gmail.com>
2020-01-12 11:15:51 +0000

    Updated examples for node

--- examples/node/server.js
+++ examples/node/server.js
@@ -6,18 +6,17 @@ var clientSecret = "clientSecret_example"; // {String} Client secret of the auth
 var clientId = "clientId_example"; // {String} Client ID of the authenticating app.
 var code = "code_example"; // {String} Secret code obtained from the authorise endpoint.
 
-// ADD YOUR TOKEN HERE TO TEST LOCALLY
-var token = ""
+var token = "";
 
 var failedTests = 0;
 var passedTests = 0;
 
 var callback = function(error, data, response) {
   if (error) {
-  	failedTests ++;
+    failedTests ++;
     // console.error(error);
-  } else {	
-  	passedTests ++;
+  } else {  
+    passedTests ++;
     //console.log(data);
   }
   console.log("Failed: " + failedTests + " Passed: " + passedTests
@@ -25,7 +24,7 @@ var callback = function(error, data, response) {
 };
 console.log("Calling the uclapi...")
 // People search (token, query, callback)
-api.searchGet(
+api.searchPeopleGet(
     token,
     "Jane",
     callback
@@ -39,42 +38,41 @@ api.resourcesDesktopsGet(
 api.roombookingsBookingsGet(
     token,
     {
-    	roomname: "Torrington (1-19) 433",
-    	roomid: "433",
-    	start_datetime: "2020-12-01T09:00:00+00:00",
-		end_datetime: "2020-12-01T09:30:00+00:00",
-		date: "20200112",
-	    siteid: "086",
-	},
+      roomname: "Torrington (1-19) 433",
+      roomid: "433",
+      start_datetime: "2020-12-01T09:00:00+00:00",
+    end_datetime: "2020-12-01T09:30:00+00:00",
+    date: "20200112",
+      siteid: "086",
+  },
     callback
 );
 // Room Bookings Get Equipment (token, roomid, siteid, callback)
 api.roombookingsEquipmentGet(
-	token,
-	// "433",
-	// "086",
-	callback
+  token,
+  "433",
+  "086",
+  callback
 )
 // Room Bookings Get Free Rooms (token, start_datetime, end_datetime, callback)
 api.roombookingsFreeroomsGet(
-	token,
-	{},
-	// "2020-12-01T09:00:00+00:00",
-	// "2020-12-01T09:30:00+00:00",
-	callback
+  token,
+  "2020-12-01T09:00:00+00:00",
+  "2020-12-01T09:30:00+00:00",
+  callback
 )
 // Room Bookings Rooms Get (token, optional, callback)
 api.roombookingsRoomsGet(
-	token,
+  token,
     {
-    	roomname: "Torrington (1-19) 433",
-    	roomid: "433",
-	    siteid: "086",
-	    sitename: "Torrington Place, 1-19",
-	    classification: "SS",
-	    capacity: "55"
-	},
-	callback
+      roomname: "Torrington (1-19) 433",
+      roomid: "433",
+      siteid: "086",
+      sitename: "Torrington Place, 1-19",
+      classification: "SS",
+      capacity: "55"
+  },
+  callback
 )
 // ALL ENDPOINTS USING OAUTH TO ADD...
 
